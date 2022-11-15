d7c729c5bf1ae0645e83753d40a79f35bfdcf2f0
Sohail Shiraj <sohail.shiraj@hotmail.com>
2022-11-14 23:23:20 -0500

    changes

--- public/electron.js
+++ public/electron.js
@@ -3,9 +3,10 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const isDev = require('electron-is-dev');
 
+let win;
 function createWindow() {
     // Create the browser window.
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 1000,
         height: 800,
         minHeight: 600,
@@ -15,6 +16,8 @@ function createWindow() {
         resizable: false,
         webPreferences: {
             nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
         },
     });
 
@@ -25,6 +28,8 @@ function createWindow() {
             ? 'http://localhost:3000'
             : `file://${path.join(__dirname, '../build/index.html')}`
     );
+    win.on("closed", () => (mainWindow = null));
+
     // Open the DevTools.
     if (isDev) {
         win.webContents.openDevTools({ mode: 'detach' });
@@ -34,8 +39,9 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+//app.whenReady().then(createWindow);
 
+app.on("ready", createWindow);
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bars to stay active until the user quits
 // explicitly with Cmd + Q.
@@ -46,7 +52,7 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
+    if (win === null) {
         createWindow();
     }
 });
