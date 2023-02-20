6ecc385c0f78085ffc4ebf6d68c6cadc7a08facb
Sohail Shiraj <sohail.shiraj@hotmail.com>
2023-02-20 14:38:33 -0500

    changes

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class HttpService {
   constructor() {
     axios.defaults.baseURL =
-      'http://127.0.0.1:5000/';
+      'http://127.0.0.1:3000/';
 
     //axios.defaults.headers.post['Content-Type'] =
     //  'application/x-www-form-urlencoded';
